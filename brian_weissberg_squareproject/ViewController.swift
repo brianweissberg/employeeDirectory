efdc19f72aa33b90e429ca1e85c63de22d91af9a
Brian Weissberg <brianweissberg@gmail.com>
2022-08-19 12:10:47 -0500

    Initial Commit

---
+++ brian_weissberg_squareproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  brian_weissberg_squareproject
+//
+//  Created by Brian Weissberg on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
